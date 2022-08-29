83c3094a12341265067228db27064ba298dbc94b
David Rossello <rossel979@morrisville.edu>
2022-08-29 13:35:27 -0400

    Introducing Variables

--- Obstaclecourse/Assets/Scripts/Mover.cs
+++ Obstaclecourse/Assets/Scripts/Mover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float fltxValue = 0f;
+    float fltyValue = 0.01f;
+    float fltzValue = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1, 0, 0);
+        transform.Translate(fltxValue, fltyValue, fltzValue);
     }
 }
