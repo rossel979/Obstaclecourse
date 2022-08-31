2e35233836db3c1e29d4096690a7602d3a01a1e5
David Rossello <rossel979@morrisville.edu>
2022-08-31 17:04:11 -0400

    practicing with methods

--- Obstaclecourse/Assets/Scripts/Mover.cs
+++ Obstaclecourse/Assets/Scripts/Mover.cs
@@ -8,15 +8,25 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+    void PrintInstruction() 
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls");
+    }
+    void MovePlayer()
+    {
         float fltxValue = Input.GetAxis("Horizontal") * Time.deltaTime * fltmovespeed;
         float fltzValue = Input.GetAxis("Vertical") * Time.deltaTime * fltmovespeed;
-        
-        transform.Translate(fltxValue,0, fltzValue);
+
+        transform.Translate(fltxValue, 0, fltzValue);
     }
 }
