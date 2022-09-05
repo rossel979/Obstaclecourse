69edc75ef40cb075afe603d94121e9dce7efcb3a
David Rossello <rossel979@morrisville.edu>
2022-09-05 13:16:08 -0400

    using GetComponent<>

--- Obstaclecourse/Assets/Scripts/Objectshit.cs
+++ Obstaclecourse/Assets/Scripts/Objectshit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Objectshit : MonoBehaviour
@@ -7,5 +8,7 @@ public class Objectshit : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Bumped into wall");
+        GetComponent<MeshRenderer>().material.color = new Color (1f,0f,1f,1f);
+        GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 1f);
     }
 }
