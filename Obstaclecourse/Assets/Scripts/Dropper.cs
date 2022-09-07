7df0c33ae0d50c7d4c04df4c89307f9efb340165
David Rossello <rossel979@morrisville.edu>
2022-09-07 12:49:49 -0400

    if statements

--- Obstaclecourse/Assets/Scripts/Dropper.cs
+++ Obstaclecourse/Assets/Scripts/Dropper.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] float flttimeToWait = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+        if(Time.time > 3) 
+        {
+            Debug.Log("3 seconds has elapsed");
+        }
     }
 }
